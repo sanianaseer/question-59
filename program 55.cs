42ec01d0f97a18b1ecdd322112673752b8637f97
Sania Naseer <muzamilnaseer03084176919@gmail.com>
2021-11-25 12:47:43 -0800

    first commit

---
+++ program 55.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            Console.WriteLine(i = 10);
+            Console.ReadLine();
+         
+
+
+
+
+        }
+    }
+}
