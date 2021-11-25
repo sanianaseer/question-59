e171c45b041bc362d8cb875b0f5314b41d300216
Sania Naseer <muzamilnaseer03084176919@gmail.com>
2021-11-25 13:25:38 -0800

    first commit

--- program 58.cs
+++ program 58.cs
@@ -1,20 +1,13 @@
 ﻿using System;
 
-namespace q55
+namespace program_55
 {
     class Program
     {
         static void Main(string[] args)
         {
             const float PI = 3.142F;
-
-            Console.WriteLine("Value of PI = "+ PI );
-            Console.ReadLine();
-         
-
-
-
-
+            Console.WriteLine("Value of PI =" + PI);
         }
     }
 }
