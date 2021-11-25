53129483128286bc2aa2c74ed308ad3937ffdd35
Sania Naseer <muzamilnaseer03084176919@gmail.com>
2021-11-25 12:25:15 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x=99;
+             
+            Console.WriteLine("Value of x = x+2" );
+            Console.ReadLine();
+         
+
+
+
+
+        }
+    }
+}
