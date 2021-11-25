0f7980876d218bd664859b98abf941fa7e4f683e
Sania Naseer <muzamilnaseer03084176919@gmail.com>
2021-11-25 13:58:08 -0800

    first commit

---
+++ program 53.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 22 / 8;
+
+            Console.WriteLine(n);
+            Console.ReadLine();
+
+            int a = 99 / 8 + 21 / 11 * 7;
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+
+            double c = 4, d = 3,p = 8, q = 2;
+            double t = q / c - p / d;
+            Console.WriteLine(t);
+            Console.ReadLine();
+
+        }
+    }
+}
